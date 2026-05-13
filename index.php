09aca24e761741c89e2c8658da6f62138c8d5503
Mario <139500537+R3tr086X@users.noreply.github.com>
2026-05-13 18:20:05 +0200

    Add database connection script in index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = "db";
+$user = "root";
+$password = getenv("ROOT_PASSWORD");
+$database = "test";
+
+echo "<h2>Conexión a MariaDB</h2>";
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    echo "<p style='color:red;'>Error de conexión: " . $conn->connect_error . "</p>";
+} else {
+    echo "<p style='color:green;'>Conexión exitosa a la base de datos</p>";
+    echo "<p>Usuario: mgarfer1604</p>";
+}
+
+?>
